e23356adc0a7a3ae77275e194396c4deeaf0db1b
Alexander Karanja <alexanderkaranja78@gmail.com>
2024-09-16 20:32:08 +0300

    :memo:

--- lib/helpers.py
+++ lib/helpers.py
@@ -67,29 +67,60 @@ def delete_department():
 
 # You'll implement the employee functions in the lab
 
-def list_employees():
-    pass
+def list_employees(employees):
+    if not employees:
+        print("No employees found.")
+    for employee in employee:
+        print(employee)
+
+def find_employee_by_name(employees, name):
+    for employee in employees:
+        if employee.name.lower() == name.lower():
+            return employee
+    return "Employee not found."
 
 
-def find_employee_by_name():
-    pass
+def find_employee_by_id(emp_id, employees):
+    for employee in employees:
+        if employee.emp_id == emp_id:
+            return employee
+    return "Employee not found."
 
 
-def find_employee_by_id():
-    pass
+def create_employee(emp_id, name, department, employees):
+    if find_employee_by_id(emp_id) == "Employee not found.":
+        new_employee = Employee(emp_id, name, department)
+        employees.append(new_employee)
+        print(f"Employee {name} created successfully.")
+    else:
+        print("An employee with this ID already exists.")
 
 
-def create_employee():
-    pass
+def update_employee(emp_id, name=None, department=None):
+    employee = find_employee_by_id(emp_id)
+    if isinstance(employee, Employee):
+        if name:
+            employee.name = name
+        if department:
+            employee.department = department
+        print(f"Employee {emp_id} update successfully.")
+    else:
+        print(employee)
 
 
-def update_employee():
-    pass
+def delete_employee(emp_id, employees):
+    employee = find_employee_by_id(emp_id)
+    if isinstance(employee, Employee):
+        employees.remove(employee)
+        print(f"Employee {emp_id} deleted successfully.")
+    else:
+        print(employee)
 
 
-def delete_employee():
-    pass
-
-
-def list_department_employees():
-    pass
+def list_department_employees(department, employees):
+    dept_employees = [emp for emp in employees if emp.department.lower() == department.lower()]
+    if dept_employees:
+        for emp in dept_employees:
+            print(emp)
+    else:
+        print(f"No employees found in the {department} department.")
